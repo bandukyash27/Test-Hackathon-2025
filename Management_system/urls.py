b2078487b7582297817454ec0724bc6484228d97
bandukyash27 <byaswanth899@gmail.com>
2025-10-23 22:16:45 +0530

    Ninja configuration

--- Management_system/urls.py
+++ Management_system/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from api.api import router as api_router
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',api_router.urls)
 ]
